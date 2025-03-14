dfdad78284239b266127509f57b9fcdb2d897ecd
Adex324 <o.joeade@gmail.com>
2025-03-14 15:48:39 +0100

    okay

--- note_app/note_app/settings.py
+++ note_app/note_app/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import dj_database_url
-from pathlib import Path
+import os
+from pathlib import Path    
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(m(x3*o)6v26pd7p+a36(=y&@ezkt(kb2y_mm==xt)e_x_tzhn'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() = "true"
 
-ALLOWED_HOSTS =[]
+ALLOWED_HOSTS =os.environ.get("ALLOWED_HOSTS").split(" ")
 
 # Application definition
 
@@ -79,8 +80,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-DATABASES["default"]=dj_database_url.parse('postgresql://note_app_django_user:SN2XjdjL7Yt9yCEfOy7urGSYGckeSgG5@dpg-cva0ju3tq21c73bph6s0-a.oregon-postgres.render.com/note_app_django')
+database_url=os.environ.get("DATABASE_URL")
+DATABASES["default"]=dj_database_url.parse(database_url)
 #postgresql://note_app_django_user:SN2XjdjL7Yt9yCEfOy7urGSYGckeSgG5@dpg-cva0ju3tq21c73bph6s0-a.oregon-postgres.render.com/note_app_django
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
